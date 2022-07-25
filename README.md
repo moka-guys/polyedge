582a1fd1bbe9cdd5c19919b3a66f18e40ad49a7e
David Brawand <dbrawand@gmail.com>
2022-07-25 12:20:45 +0100

    implemented polyN purity measurement, addressed comment of code-review

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 Finds variants at the edge of a poly that is is varying length.
 
+It is currently hard-coded to find the varying base at the 5'-end of intron 5 in MSH2.
+
+## Usage 
+
+Simply run with an indexed BAM file as first argument, like:
+```
+python polyedge.py your_bam_file.bam
+```
+
+NB: An optional second argument can be used to modify the default anchor length of 2 (number of aligned bases required on either side of the repeat). It is _not_ recommended to change this.
+
+## Output
+
+The result is a table written to STDOUT, like this:
+```
+A  33 204 0.38 23.52 2.62 25 1.00
+T  20 328 0.62 25.51 2.34 27 0.99
+|  |  |   |    |     |    |  +- Average purity of polyN repeat
+|  |  |   |    |     |    +- Mode of poly length
+|  |  |   |    |     +- Standard deviation of poly length
+|  |  |   |    +- Mean poly length
+|  |  |   +- Fraction of reads
+|  |  +- Read Count
+|  +- Mean quality if first base
++- First base of poly repeat allele
+```
+
--- polyedge.py
+++ polyedge.py
@@ -2,7 +2,7 @@ import pysam
 import sys
 import re
 import statistics
-from collections import defaultdict
+from collections import defaultdict, Counter
 from functools import reduce
 
 CHROM = "2"
@@ -13,7 +13,9 @@ ANCHOR_LENGTH = 2
 
 def main(bamfile):
     # list of poly lenght counts by first base allele
-    counts, quals = defaultdict(list), defaultdict(list)
+    counts = defaultdict(list)  # lengths of repeat given allele (one per read)
+    quals = defaultdict(list)  # quality of first base
+    purity = defaultdict(list)  # most common base count in poly w/o first base
     # open BAM file
     reader = pysam.Samfile(bamfile, 'rb')
     # fetch reads intersecting the poly repeat
@@ -25,6 +27,7 @@ def main(bamfile):
         # select that span poly with defined ANCHOR sequence length
         if read.reference_start < roi_start and roi_end < read.reference_end:
             # find sequence segment with poly and anchor sequence
+            # -> first matching position in the query->reference mapping
             seq_start = next(x for x in read.get_aligned_pairs()
                              if x[1] == roi_start)
             seq_end = next(x for x in read.get_aligned_pairs()
@@ -34,7 +37,8 @@ def main(bamfile):
                 seq_segment = read.query_sequence[seq_start[0]:seq_end[0]]
                 first_base_pos = seq_start[0]+ANCHOR_LENGTH
                 first_base_qual = read.query_qualities[first_base_pos]
-                # extract poly sequence assuming no indel in anchor sequence)
+                # extract poly sequence (excluding anchor bases)
+                # assuming no indel in anchor sequence
                 regex = r'.{' + re.escape(str(ANCHOR_LENGTH)) + \
                     r'}(.+).{' + re.escape(str(ANCHOR_LENGTH)) + r'}'
                 seq_match = re.match(regex, seq_segment)
@@ -42,6 +46,11 @@ def main(bamfile):
                     # partition poly by first base allele
                     allele = seq_match.group(1)[0]
                     counts[allele].append(len(seq_match.group(1)))
+                    # count polyN purity
+                    remainder_poly = Counter(seq_match.group(1)[1:])
+                    poly_most_abundant = remainder_poly.most_common()[0][1]
+                    purity[allele].append(poly_most_abundant)
+                    # quality of first base of ROI (allele in question)
                     quals[allele].append(first_base_qual)
     # calculate statistics
     total_count = reduce(lambda a, c: a + len(c), counts.values(), 0)
@@ -52,8 +61,11 @@ def main(bamfile):
                 len(counts[allele])/total_count,
                 statistics.mean(counts[allele]),
                 statistics.stdev(counts[allele]),
-                statistics.mode(counts[allele]))
-        print('{:>} {:4n} {:2n} {:.2f} {:2.2f} {:.2f} {:2n}'.format(*data))
+                statistics.mode(counts[allele]),
+                sum(purity[allele])/(sum(counts[allele])-len(counts[allele]))
+                )
+        print('{:>} {:4n} {:2n} {:.2f} {:2.2f} {:.2f} {:2n} {:.2f}'
+              .format(*data))
 
 
 if __name__ == "__main__":
